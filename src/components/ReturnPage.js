8c4f3a228c7c557a741137a161b183f72abe9456
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-02-09 12:01:22 -0400

    Add funcionality to return buttom

--- src/components/ReturnPage.js
+++ src/components/ReturnPage.js
@@ -1,9 +1,31 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import arrowRightLong from '../assets/icons/arrowRightLong.png';
 
+const routes = {
+  "/servicios/consultas": true,
+  "/servicios/maternidad": true,
+  "/servicios/especialidades": true,
+  "/servicios/cuidados-intensivos": true,
+  "/servicios/internacion": true,
+  "/servicios/emergencias": true,
+  "/servicios/estudios-diagnosticos": true,
+  "/servicios/cuidado-y-prevencion": true,
+};
+
 const ReturnPage = () => {
+  const navigate = useNavigate();
+  const handleReturn = () => {
+    if (routes[window.location.pathname]) {
+      navigate('/servicios');
+    } else {
+      navigate('/');
+    }
+  };
+
   return (
     <div className="return-container">
-      <button type="button">
+      <button type="button" onClick= { handleReturn }>
         <img src={arrowRightLong} alt="Fecha" />
         <p>Volver</p>
       </button>
